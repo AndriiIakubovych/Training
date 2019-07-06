477c9716e7f6bbcc4d84a0470c725aeef5b44005
Andrii <andrii.iakubovych@gmail.com>
2019-07-06 12:48:08 +0300

    Commit

---
+++ Job Interview/Task 8/StringSort/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StringSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.Write("Введите строку: ");
+            str = Console.ReadLine();
+            Console.WriteLine("\nПреобразованная строка: " + Sort(str));
+            Console.ReadKey();
+        }
+
+        static string Sort(string str)
+        {
+            char[] array = str.ToCharArray();
+            Array.Sort(array);
+            Array.Reverse(array);
+            return new string(array);
+        }
+    }
+}
